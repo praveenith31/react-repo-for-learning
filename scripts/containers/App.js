18535c313129b73c59ad71daba0a211f42d48589
SAPIENT\psha78 <psharma81@sapient.com>
2017-05-05 19:39:06 +0530

    - Removed files

--- scripts/containers/App.js
+++ scripts/containers/App.js
@@ -10,7 +10,6 @@ import MeContainer from '../containers/MeContainer';
 import ModalContainer from '../containers/ModalContainer';
 import PlayerContainer from '../containers/PlayerContainer';
 import SongContainer from '../containers/SongContainer';
-import SongsContainer from '../containers/SongsContainer';
 import UserContainer from '../containers/UserContainer';
 
 const propTypes = {
@@ -22,52 +21,12 @@ const propTypes = {
 };
 
 class App extends Component {
-  componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch(initEnvironment());
-    dispatch(initAuth());
-    dispatch(initNavigator());
-  }
-
-  renderContent() {
-    const { path } = this.props;
-    switch (path[0]) {
-      case 'songs':
-        switch (path.length) {
-          case 1:
-            return <SongsContainer />;
-          case 2:
-            return <SongContainer />;
-          default:
-            return null;
-        }
-      case 'users':
-        return <UserContainer />;
-      case 'me':
-        return <MeContainer />;
-      default:
-        return null;
-    }
-  }
 
   render() {
-    const { height, isMobile, width } = this.props;
-    if (isMobile) {
-      return (
-        <div className="mobile" style={{ height: `${height}px`, width: `${width}px` }}>
-          <PlayerContainer />
-          {this.renderContent()}
-          <NavContainer />
-        </div>
-      );
-    }
 
     return (
       <div>
         <NavContainer />
-        {this.renderContent()}
-        <PlayerContainer />
-        <ModalContainer />
       </div>
     );
   }
@@ -75,18 +34,5 @@ class App extends Component {
 
 App.propTypes = propTypes;
 
-function mapStateToProps(state) {
-  const { environment, navigator } = state;
-  const { height, isMobile, width } = environment;
-  const { path } = navigator.route;
 
-  return {
-    height,
-    isMobile,
-    path,
-    width,
-  };
-}
-
-
-export default connect(mapStateToProps)(App);
+export default App;
